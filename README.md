81a79b9b0e7cb7114a0e08b559852767b513d445
Al Amin <almin.cont@gmail.com>
2022-11-01 14:01:02 +0600

    add env read me

---
+++ README.md
@@ -0,0 +1,8 @@
+# stickk api
+
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file
+
+1. `MONGODB_CONNECTION`
+2. `MY_SECRET_TOKEN`
--- server.js
+++ server.js
@@ -13,11 +13,12 @@ const app = express();
 
 //! middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
 //! server route
 app.get("/", async (req, res) => {
-  res.json("hello from stickk server");
+  res.json({api:"hello from stickk server 📕"});
 });
 
 //! use router
@@ -28,10 +29,10 @@ app.use("/api/user/profile", userProfileRouter);
 mongoose
   .connect(`${process.env.MONGODB_CONNECTION}`)
   .then(() => {
-    console.log("database connected");
-    const port = process.env.PORT || 1000;
+    // console.log("database connected");
+    const port = process.env.PORT || 4000;
     app.listen(port, () => {
-      console.log(`server is running: ${port}`);
+      console.log(`server is running: ${port} 😎`);
     });
   })
   .catch((err) => {
